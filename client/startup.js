4b39c695b83a197a1142d7061b512f02f2b1b06b
Paul Cleary <pauljohncleary@gmail.com>
2014-10-06 22:33:45 +0100

    started configuring login

--- client/startup.js
+++ client/startup.js
@@ -4,9 +4,9 @@ Meteor.startup(function () {
     privacyUrl: '/privacy-policy',     // if set adds link to privacy policy and 'you agree to ...' on sign-up page
     termsUrl: '/terms-of-use',         // if set adds link to terms  'you agree to ...' on sign-up page
     homeRoute: '/',                    // mandatory - path to redirect to after sign-out
-    dashboardRoute: '/all',      // mandatory - path to redirect to after successful sign-in
+    dashboardRoute: '/',      // mandatory - path to redirect to after successful sign-in
     //profileRoute: 'profile',
-    passwordSignupFields: 'EMAIL_ONLY',
+    //passwordSignupFields: 'EMAIL_ONLY',
     //showSignupCode: true,
     showOtherLoginServices: true,      // Set to false to hide oauth login buttons on the signin/signup pages. Useful if you are using something like accounts-meld or want to oauth for api access
     //extraSignUpFields: [{             // Add extra signup fields on the signup page
--- client/talkapp.js
+++ client/talkapp.js
@@ -1,13 +1,13 @@
 // counter starts at 0
 Session.setDefault("counter", 0);
 
-Template.hello.helpers({
+Template.loading.helpers({
   counter: function () {
     return Session.get("counter");
   }
 });
 
-Template.hello.events({
+Template.loading.events({
   'click button': function () {
     // increment the counter when button is clicked
     Session.set("counter", Session.get("counter") + 1);
