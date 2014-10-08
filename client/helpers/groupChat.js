691e4c1ad1cf3065e8aecb0374a973cc62d921e6
Paul Cleary <pauljohncleary@gmail.com>
2014-10-08 22:39:09 +0100

    added submit form for chatting

--- client/helpers/groupChat.js
+++ client/helpers/groupChat.js
@@ -1,8 +1,23 @@
 Template.groupChat.helpers({
   members: function () {
-    return Groups.findOne().members;
+    var group = Groups.findOne();
+    Session.set('groupId', group._id);
+    return group.members;
   },
   messages: function() {
     return Groups.findOne().messages;
+  }
+});
+
+
+Template.groupChat.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var message = {
+      message: $(e.target).find('[name=message]').val()
+    };
+
+    Groups.update({ _id : Session.get('groupId') }, { $push: { messages: message} });
   }
 });
